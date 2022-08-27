dd87b2f068cc26d4079e94433966f6f604f14b8e
matheus lima <matheusmt31@gmail.com>
2022-08-26 23:16:10 -0300

    feat: adicionado viewBinding

--- app/src/main/java/br/com/android/pomodoro/TimerFragment.kt
+++ app/src/main/java/br/com/android/pomodoro/TimerFragment.kt
@@ -5,10 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewbinding.ViewBinding
+import br.com.android.pomodoro.databinding.FragmentTimerBinding
 
 
 class TimerFragment : Fragment() {
 
+    private lateinit var viewBinding: FragmentTimerBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -16,9 +19,9 @@ class TimerFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_timer, container, false)
+    ): View {
+        viewBinding = FragmentTimerBinding.inflate(inflater,container,false)
+        return viewBinding.root
     }
 
 }
